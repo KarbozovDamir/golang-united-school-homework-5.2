f5844bd4007e8dbf67ba7aa79aa83ba14ba091e6
KarbozovDamir <damirkap89@mail.ru>
2022-05-30 01:23:19 +0600

    add: cache.go

--- cache.go
+++ cache.go
@@ -3,21 +3,56 @@ package cache
 import "time"
 
 type Cache struct {
+	kh map[string]cache
+}
+
+type cache struct {
+	value    string
+	deadline time.Time
 }
 
 func NewCache() Cache {
-	return Cache{}
+
+	return Cache{
+		kh: make(map[string]cache),
+	}
 }
 
-func (receiver) Get(key string) (string, bool) {
+func (c Cache) Get(key string) (string, bool) {
+
+	if a, ok := c.kh[key]; ok {
+		if a.deadline.IsZero() || time.Now().Before(a.deadline) {
+
+			return a.value, true
+		}
+	}
+	return "", false
 
 }
 
-func (receiver) Put(key, value string) {
+func (c *Cache) Put(key, value string) {
+	c.kh[key] = cache{
+
+		value:    value,
+		deadline: time.Time{},
+	}
 }
 
-func (receiver) Keys() []string {
+func (c Cache) Keys() []string {
+	res := []string{}
+	for k, a := range c.kh {
+		if a.deadline.IsZero() || time.Now().Before(a.deadline) {
+
+			res = append(res, k)
+		}
+	}
+	return res
 }
 
-func (receiver) PutTill(key, value string, deadline time.Time) {
+func (c *Cache) PutTill(key, value string, deadline time.Time) {
+	c.kh[key] = cache{
+
+		value:    value,
+		deadline: deadline,
+	}
 }
